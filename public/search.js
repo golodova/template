652c9f5886086b9b0d6f8171a2d226907bb6bca6
golodova <golodn03@mail.ru>
2025-05-18 21:57:23 +0300

    Диплом.Верстка

--- public/search.js
+++ public/search.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  
   // Обработчик навигации
   navItems.forEach(item => {
     item.addEventListener('click', function(e) {
